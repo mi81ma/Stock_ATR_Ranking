15f58850b558f6649709b217257fc2c1040d1aeb
mi81ma <mi81ma@gmail.com>
2019-06-23 15:15:19 +0800

    Initial Commit

---
+++ Stock_ATR_Ranking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock_ATR_Ranking
+//
+//  Created by masato on 23/6/2019.
+//  Copyright © 2019 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stock_ATR_RankingTests/Stock_ATR_RankingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stock_ATR_RankingTests.swift
+//  Stock_ATR_RankingTests
+//
+//  Created by masato on 23/6/2019.
+//  Copyright © 2019 masato. All rights reserved.
+//
+
+import XCTest
+@testable import Stock_ATR_Ranking
+
+class Stock_ATR_RankingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
